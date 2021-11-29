8ef5a2ae35039b6a1f07f129d10978431c2b7833
victor <valarcon@gmail.com>
2021-11-29 19:24:49 +0100

    prova

---
+++ practica0/src/main/java/dam2/valarcon/practica0/App.java
@@ -0,0 +1,13 @@
+package dam2.valarcon.practica0;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
